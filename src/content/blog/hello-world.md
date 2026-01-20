1b5c0835cdf1866cbb9671e7c8c16ea9dbb5218f
Thomas Manch <thomasmanch@Thomass-MacBook-Pro.local>
2026-01-20 21:44:21 +1300

    Initial website

---
+++ src/content/blog/hello-world.md
@@ -0,0 +1,6 @@
+---
+title: "Hello world"
+pubDate: 2026-01-20
+---
+This is my first post.
+
---
+++ src/data/writing.ts
@@ -0,0 +1,8 @@
+export const writing = [
+  {
+    title: "Example story title",
+    outlet: "BusinessDesk",
+    date: "2026-01-15",
+    url: "https://businessdesk.co.nz/journalist/thomas-manch"
+  }
+];
